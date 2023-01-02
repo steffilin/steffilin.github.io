1a85b0f183b18668dade9ea299353402743d641e
Steffi Lin <steffilin@berkeley.edu>
2023-01-01 22:37:46 -0800

    added link to resume

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Contact.css';
-import Message from './Message'
+import Message from './Message';
+import Pdf from './Resume/Steffi-Lin-Resume.pdf';
 
 const Contact = () => {
     // const span = document.getElementById("email");
@@ -22,14 +23,15 @@ const Contact = () => {
             <h3 id="contact-bookmark">Contact Info</h3>
         </div>
         <div className="contact-info">
-            <p>Send me a message!</p>
+            {/* <p>Send me a message!</p> */}
             <Message />
             <p>Or connect with me through the links below:</p>
             <p id="email">steffi2002.lin@gmail.com  </p> 
-            <p id="contact-links">|  <a href ="https://www.linkedin.com/in/steffilin/"
+            <p id="contact-links"><a href ="https://www.linkedin.com/in/steffilin/"
                 target="blank">Linkedin</a>  |  <a href ="https://github.com/steffilin"
                 target="blank">Github</a>  |  <a href="https://www.instagram.com/steffilinart/"
-                target="blank">Art Instagram</a>
+                target="blank">Art Instagram</a>  |  <a href = {Pdf} 
+                target ="blank">Resume</a>
             </p>
         </div>
     
--- src/Message.js
+++ src/Message.js
@@ -1,20 +1,20 @@
 // import React from "react";
 import React, { useState } from 'react';
 
-function Message() { 
+const Message = () => { 
 
     const [msg, setMsg] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert('Your message was : ${msg}')
+        alert('Your message : "' + msg + '" was sent to Steffi!');
     }
 
     return(
         <div>
             <form onSubmit={handleSubmit}>
                 <label>
-                    Send me a message! <br></br>
+                    Say hello! <br></br>
                     <input type="text" value={msg} onChange={(e) => setMsg(e.target.value)}></input>
                 </label>
                 <input type="submit" />
