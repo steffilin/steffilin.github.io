84b72825f7646939ace43234548a17238983b5bd
steffilin <steffi2002.lin@gmail.com>
2024-05-28 19:45:55 +0200

    added content to education section. haven't styled yet

--- src/Education.js
+++ src/Education.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from 'react';
 
 import "./Education.css";
+import berkeleyimg from "./images/berkeley.png";
 
 
 const Education = () => {
@@ -10,7 +11,15 @@ const Education = () => {
             <div className="content">
                 <div id="linedetail"></div>
                 <h3 id="proj-bookmark" className="head-placement">EDUCATION</h3>
-                
+                <div className="education-text-container">
+                    <div>
+                        <p style={{fontWeight:"500"}}>University of California, Berkeley</p>
+                        <p>College of Letters and Science</p>
+                        <p>Areas of study: Computer Science, Data Science</p>
+                    </div>
+                    
+                    <img src={berkeleyimg}></img>
+                </div>
                 {/* <ChangeExp/> */}
                         
             </div>
