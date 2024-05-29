c501bdc93747d10920e51a68cb7ebc01759d7ee4
steffilin <steffi2002.lin@gmail.com>
2024-05-29 23:17:22 +0200

    removed contact tab

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <ul>
                 <li><a href="index.html">Home</a></li>
                 <li><a href="#proj-bookmark">Projects</a></li>
-                <li><a href="#contact-bookmark">Contact</a></li>
+                {/* <li><a href="#contact-bookmark">Contact</a></li> */}
                 {/* <li><a href="art.html">Art</a></li> */}
             </ul>
         </nav>
